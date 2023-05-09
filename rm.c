273ae0579aac23dfafb5a3cfa7c1aba328420bc1
Onnrr <onur.uysal@ug.bilkent.edu.tr>
2023-05-09 22:31:27 +0300

    lock request falan filan

--- rm.c
+++ rm.c
@@ -11,7 +11,13 @@ int N;   // number of processes
 int M;   // number of resource types
 int ExistingRes[MAXR]; // Existing resources vector
 int MaxDemand[MAXP][MAXR] = {0};
+int Allocation[MAXP][MAXR] = {0};
+int Need[MAXP][MAXR] = {0};
 int threads[MAXP] = {0};
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
 
 //..... other definitions/variables .....
 //.....
@@ -19,8 +25,7 @@ int threads[MAXP] = {0};
 
 // end of global variables
 
-int rm_thread_started(int tid)
-{
+int rm_thread_started(int tid) {
     int ret = 0;
 
     if (tid < N) {
@@ -35,8 +40,7 @@ int rm_thread_started(int tid)
 }
 
 
-int rm_thread_ended()
-{
+int rm_thread_ended() {
     int ret = 0;
     int id = getThread(pthread_self());
     if (threads[id] == -1) {
@@ -49,8 +53,7 @@ int rm_thread_ended()
 }
 
 
-int rm_claim (int claim[])
-{
+int rm_claim (int claim[]) {
     int ret = 0;
 
     if (DA) {
@@ -71,8 +74,7 @@ int rm_claim (int claim[])
 }
 
 
-int rm_init(int p_count, int r_count, int r_exist[],  int avoid)
-{
+int rm_init(int p_count, int r_count, int r_exist[],  int avoid) {
     int i;
     int ret = 0;
     
@@ -100,6 +102,73 @@ int rm_init(int p_count, int r_count, int r_exist[],  int avoid)
 int rm_request (int request[])
 {
     int ret = 0;
+
+    if (DA) {
+        int finish[N];
+        for (int i = 0; i < N; i++) {
+            finish[i] = 0;
+        }
+        int work[M];
+        for (int i = 0; i < M; i++) {
+            work[i] = ExistingRes[i];
+        }
+        
+        int found;
+        int i, j;
+        do {
+            found = 0;
+            for (i = 0; i < N; i++) {
+                if (finish[i] == 0) {
+                    int can_allocate = 1;
+                    for (j = 0; j < N; j++) {
+                        if (Need[i][j] > work[j]) {
+                            can_allocate = 0;
+                            break;
+                        }
+                    }
+
+                    if (can_allocate) {
+                        found = 1;
+                        finish[i] = 1;
+                        for (j = 0; j < M; j++) {
+                            work[j] += Allocation[i][j];
+                        }
+                    }
+                }
+            }
+        } while (found);
+
+        // Check if all processes are finished
+        for (i = 0; i < N; i++) {
+            if (finish[i] == 0) {
+                // TODO Sleep
+            }
+        }
+
+    }
+    else {
+        int available = 0;
+        while (available == 0) {
+            available = 1;
+            for (int i = 0; i < M; i++) {
+                if (ExistingRes[i] < request[i]) {
+                    available = 0;
+                    break;
+                }
+            }
+            if (available == 0) {
+                pthread_cond_wait(&cv, &lock);
+            }
+            else {
+                for (int i = 0; i < M; i++) {
+                    ExistingRes[i] -= request[i];
+                }
+                pthread_mutex_unlock(&lock);
+                return 0;
+            }
+            pthread_mutex_unlock(&lock);
+        }
+    }
     
     return(ret);
 }
